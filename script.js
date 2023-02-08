e0e79cf63656a7d6847db08efcc7facad754e7ff
AlexJFisher94 <alexjfisher1994@gmail.com>
2023-02-08 17:18:50 -0500

    Added query selectors and code to hide/remove hidden class on nav menu elements

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".nav-bar");
 const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
     menuOpen = true;
+    navMenu.classList.remove("hidden");
+    navBar.classList.add("hidden");
   } else {
     menuBtn.classList.remove("open");
     menuOpen = false;
+    navMenu.classList.add("hidden");
+    navBar.classList.remove("hidden");
   }
 });
 
